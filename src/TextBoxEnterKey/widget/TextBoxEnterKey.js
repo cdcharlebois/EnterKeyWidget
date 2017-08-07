a7a5999759e44ab060e240450a75f1ff72d9c6ad
Emile Broeders <emile.broeders@finaps.nl>
2017-08-07 14:50:12 +0200

    Only formatting

--- src/TextBoxEnterKey/widget/TextBoxEnterKey.js
+++ src/TextBoxEnterKey/widget/TextBoxEnterKey.js
@@ -7,15 +7,16 @@
     @version   : 1.0
     @author    : Christiaan Westgeest
     @date      : Fri, 26 Feb 2016 10:42:29 GMT
-    @copyright : 
-    @license   : 
+    @copyright :
+    @license   :
 
     Documentation
     ========================
     Describe your widget here.
 */
 
-// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+// Required module list. Remove unnecessary modules, you can always get them
+// back from the boilerplate.
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
@@ -36,13 +37,16 @@ define([
     "dojo/_base/event",
     "TextBoxEnterKey/lib/jquery-1.11.2",
     "dojo/text!TextBoxEnterKey/widget/template/TextBoxEnterKey.html"
-], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoKeys, dojoAttr, dojoEvent, _jQuery, widgetTemplate) {
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp,
+  dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang,
+  dojoText, dojoHtml, dojoKeys, dojoAttr, dojoEvent, _jQuery, widgetTemplate) {
     "use strict";
 
     var $ = _jQuery.noConflict(true);
 
     // Declare widget's prototype.
-    return declare("TextBoxEnterKey.widget.TextBoxEnterKey", [ _WidgetBase, _TemplatedMixin ], {
+    return declare("TextBoxEnterKey.widget.TextBoxEnterKey", [ _WidgetBase,
+        _TemplatedMixin ], {
         // _TemplatedMixin will create our dom node using this HTML template.
         templateString: widgetTemplate,
 
@@ -58,97 +62,105 @@ define([
         inputValue: "",
         async: "",
         placeholder: "",
-        
 
-        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+
+        // Internal variables. Non-primitives created in the prototype are
+        // shared between all widget instances.
         _contextObj: null,
         _alertDiv: null,
 
-        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        // dojo.declare.constructor is called to construct the widget instance.
+        // Implement to initialize non-primitive properties.
         constructor: function() {
             // Uncomment the following line to enable debug messages
             //logger.level(logger.DEBUG);
             logger.debug(this.id + ".constructor ");
         },
 
-        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        // dijit._WidgetBase.postCreate is called after constructing the
+        // widget. Implement to do extra setup work.
         postCreate: function() {
             logger.debug(this.id + ".postCreate ");
-            this.connect(this.inputBox, "onkeyup", dojoLang.hitch(this, this.onEnterClick));                
+            this.connect(this.inputBox, "onkeyup", dojoLang.hitch(this,
+                  this.onEnterClick));
             if (!this._isEmptyString(this.placeholder)) {
-                    dojoAttr.set(this.inputBox, "placeholder", this.placeholder);
-                }
+                    dojoAttr.set(this.inputBox, "placeholder",
+                        this.placeholder);
+            }
         },
 
-        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        // mxui.widget._WidgetBase.update is called when context is changed or
+        // initialized. Implement to re-render and / or fetch data.
         update: function(obj, callback) {
-            logger.debug(this.id + ".update ");
-            this.obj = obj;                      
-            if (this.obj) {
-                // in some cases update may be called with a null obj
-                this.changeInput();
-            }
+          logger.debug(this.id + ".update ");
+          this.obj = obj;
+          if (this.obj) {
+            // in some cases update may be called with a null obj
+            this.changeInput();
+          }
 
-            if (callback) {
-                callback();
-            }
+          if (callback) {
+            callback();
+          }
         },
 
-        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is
+        // destroyed. Implement to do special tear-down work.
         uninitialize: function() {
           logger.debug(this.id + ".uninitialize ");
-            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
-        },
-        
-           changeInput: function() {
-            this.inputBox.value = this.obj.get(this.inputValue);
-        },
-        
-        onEnterClick: function(event) {
-            console.log(event.key);
-            this.obj.set(this.inputValue, this.inputBox.value);
-            if (event.keyCode == dojoKeys.ENTER) {
-             if (this.mfToExecute !== "") {  
-                 this.executeMicroflow(this.mfToExecute, this.async, this.progressBar);
-            }
-         }
-     },
-        
-                _isEmptyString: function (str) {
-            return (!str || 0 === str.trim().length);
+          // Clean up listeners, helper objects, etc. There is no need to
+          // remove listeners added with this.connect / this.subscribe /
+          // this.own.
         },
 
-        
-         executeMicroflow : function (mf, async, showProgress) {
-            if (mf && this.obj) {
-                if (showProgress) {
-                var isModal = true; var pid = mx.ui.showProgress(this.progressMsg, isModal);
-                    }
-                mx.data.action({
-                    async : async,  
-                     store: {
-                       caller: this.mxform 
-                    },
-                    params: {
-                        actionname  : mf,
-                        applyto     : "selection",
-                        guids       : [this.obj.getGuid()],
-                        
-                    },
-                    callback: dojoLang.hitch(this, function () {
-                        this.changeInput(); 
-                        if (showProgress) {
-                        mx.ui.hideProgress(pid);
-                        }
-                    }),
-                    error: function () {
-                        logger.error("TextBoxEnterKey.widget.TextBoxEnterKey.triggerMicroFlow: XAS error executing microflow");
-                        if (showProgress) {   
-                        mx.ui.hideProgress(pid);
-                        }
-                    }
-                });
+        changeInput: function() {
+          this.inputBox.value = this.obj.get(this.inputValue);
+        },
+
+        onEnterClick: function(event) {
+          console.log(event.key);
+          this.obj.set(this.inputValue, this.inputBox.value);
+          if (event.keyCode == dojoKeys.ENTER) {
+            if (this.mfToExecute !== "") {
+              this.executeMicroflow(this.mfToExecute, this.async,
+                  this.progressBar);
             }
+          }
+       },
+
+       _isEmptyString: function (str) {
+          return (!str || 0 === str.trim().length);
+        },
+
+        executeMicroflow : function (mf, async, showProgress) {
+          if (mf && this.obj) {
+            if (showProgress) {
+              var isModal = true; var pid =
+                mx.ui.showProgress(this.progressMsg, isModal);
+            }
+            mx.data.action({
+              async : async,
+              store: {
+                caller: this.mxform
+              },
+              params: {
+                actionname  : mf,
+                applyto     : "selection",
+                guids       : [this.obj.getGuid()],
+              },
+              callback: dojoLang.hitch(this, function () {
+                this.changeInput();
+                if (showProgress) {
+                  mx.ui.hideProgress(pid);
+                }
+              }),
+              error: function () {
+                if (showProgress) {
+                  mx.ui.hideProgress(pid);
+                }
+              }
+            });
+          }
         }
     });
 });
